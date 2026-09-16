31296bc6cd3eb42f259a7902de57d46e6afe8603
jenil.j <jjeniljjjj@gmail.com>
2026-09-16 09:11:13 +0000

    dockerize spring boot mysql application

---
+++ src/main/java/com/jenil/app/UserController.java
@@ -0,0 +1,19 @@
+package com.jenil.app;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/jenil/app/UserRepository.java
@@ -0,0 +1,6 @@
+package com.jenil.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
